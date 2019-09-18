982403de0c0e2a7cf8c089727d804f63af69ea0a
MikeO <52804278+CQUni-Mike@users.noreply.github.com>
2019-09-18 10:51:32 +1000

    Week 9 - wip2

--- Week 09 - Vehicles, Cars, Trucks - Part 06/FileReadSampleDemoIncomplete.java
+++ Week 09 - Vehicles, Cars, Trucks - Part 06/FileReadSampleDemoIncomplete.java
@@ -10,6 +10,10 @@ try
 
 	while(inFile.hasNext()) // more data ?
 	{
+		// NOTE:
+		// The order you write the data needs to be exactly
+		// the same as the order in which read the data !!
+
 		String field1 = inFile.nextLine();
 		String field2 = inFile.nextLine();
 		int    field3 = inFile.nextInt();
